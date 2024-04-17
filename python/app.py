26ebedc6785b032f4185bc4cc72cc68d10e6d2d0
Zalessky <vvzlssk32@gmail.com>
2024-04-17 16:36:04 +0300

    back to the start

--- python/app.py
+++ python/app.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template
-from datetime import datetime
-
+# app.py
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def countdown():
-    project_due_date = datetime(2024, 5, 12)
-    current_date = datetime.now()
-    time_difference = project_due_date - current_date
-    if time_difference.total_seconds() <= 0:
-        return render_template('project_due.html')
-    else:
-        remaining_days = time_difference.days
-        return render_template('countdown.html', days=remaining_days, image_url='/static/img1.jpeg')
+def hello_world():
+    return 'Hello World! from Vladimir Zalessky, BSUIR, FIS <3.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
