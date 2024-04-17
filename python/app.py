47478aacb1df92a1a847bad36b156eb7ab10da15
Zalessky <vvzlssk32@gmail.com>
2024-04-17 16:56:03 +0300

    Develop new feature Countdown: add 2 html pages, image and modify the app.py file

--- python/app.py
+++ python/app.py
@@ -1,10 +1,18 @@
-# app.py
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! from Vladimir Zalessky, BSUIR, FIS <3.'
+def countdown():
+    project_due_date = datetime(2024, 5, 12)
+    current_date = datetime.now()
+    time_difference = project_due_date - current_date
+    if time_difference.total_seconds() <= 0:
+        return render_template('project_due.html')
+    else:
+        remaining_days = time_difference.days
+        return render_template('countdown.html', days=remaining_days, image_url='/static/img1.jpeg')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
